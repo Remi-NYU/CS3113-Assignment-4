63c5af60f2fc595f749d0b6afe23b93522108881
AnnaXT <annateng8000@gmail.com>
2023-02-23 15:42:09 -0500

    Implemented spawner into scene
    added level spawners into the levels

--- Assets/Script/Anna Scripts/EnemySpawner1.cs
+++ Assets/Script/Anna Scripts/EnemySpawner1.cs
@@ -14,7 +14,7 @@ public class EnemySpawner1 : MonoBehaviour
     {
         yield return new WaitForSeconds(3f);
         for(int i = 0; i < 3; i++){
-            Vector2 spawnPos = new Vector2(Random.Range(18,23), 4.5f);
+            Vector2 spawnPos = new Vector2(Random.Range(3,8), 4.5f);
             Instantiate(enemy1PreFab, spawnPos, Quaternion.identity);
             yield return new WaitForSeconds(2f);
 
@@ -22,7 +22,7 @@ public class EnemySpawner1 : MonoBehaviour
 
         yield return new WaitForSeconds(3f);
 
-        for(int i = 18; i < 23; i += 2){
+        for(int i = 3; i < 8; i += 2){
             Vector2 spawnPos = new Vector2(i, 4.5f);
             Instantiate(enemy1PreFab, spawnPos, Quaternion.identity);
             yield return new WaitForSeconds(2f);
@@ -32,7 +32,7 @@ public class EnemySpawner1 : MonoBehaviour
         yield return new WaitForSeconds(5f);
 
         for(int i = 0; i < 3; i++){
-            Vector2 spawnPos = new Vector2(16.5f, Random.Range(0,2f));
+            Vector2 spawnPos = new Vector2(2.5f, Random.Range(0,2f));
             Instantiate(enemy2PreFab, spawnPos, Quaternion.identity);
             yield return new WaitForSeconds(2f);
 
@@ -41,8 +41,8 @@ public class EnemySpawner1 : MonoBehaviour
         yield return new WaitForSeconds(5f);
 
         for(int i = 0; i < 3; i++){
-            Vector2 spawnPos = new Vector2(Random.Range(18,23), 4.5f);
-            Vector2 spawnPos2 = new Vector2(Random.Range(18,23), 4.5f);
+            Vector2 spawnPos = new Vector2(Random.Range(3,8), 4.5f);
+            Vector2 spawnPos2 = new Vector2(Random.Range(3,8), 4.5f);
             Instantiate(enemy1PreFab, spawnPos, Quaternion.identity);
             Instantiate(enemy3PreFab, spawnPos2, Quaternion.identity);
             yield return new WaitForSeconds(5f);
@@ -51,7 +51,7 @@ public class EnemySpawner1 : MonoBehaviour
 
         yield return new WaitForSeconds(3f);
 
-        for(int i = 18; i < 23; i += 2){
+        for(int i = 3; i < 8; i += 2){
             Vector2 spawnPos = new Vector2(i, 4.5f);
             Instantiate(enemy3PreFab, spawnPos, Quaternion.identity);
             yield return new WaitForSeconds(2f);
